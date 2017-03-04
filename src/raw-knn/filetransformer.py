3aa73d3d645ec8f55927bd7e9c469e5e1bc11caa
Heda Wang <whd.thu@gmail.com>
2017-03-04 14:54:41 +0800

    add gzstream, add knn (not finished), make starter code a submodule

---
+++ src/raw-knn/filetransformer.py
@@ -0,0 +1,60 @@
+import sys
+import tensorflow as tf
+
+def read_and_decode_single_example(filepattern, additional_features, feature_sizes):
+	# first construct a queue containing a list of filenames.
+	# this lets a user split up there dataset in multiple files to keep
+	# size down
+	filelist = tf.gfile.Glob(filepattern)
+	if not filelist:
+		print >> sys.stderr, "no files found in", filepattern
+	filename_queue = tf.train.string_input_producer(filelist, num_epochs=1, shuffle = False)
+
+	# Unlike the TFRecordWriter, the TFRecordReader is symbolic
+	reader = tf.TFRecordReader()
+
+	# One can read a single serialized example from a filename
+	# serialized_example is a Tensor of type string.
+	_, serialized_example = reader.read(filename_queue)
+
+	# The serialized example is converted back to actual values.
+	# One needs to describe the format of the objects to be returned
+	feature_map = {"video_id": tf.FixedLenFeature([], tf.string), 
+		"labels": tf.VarLenFeature(tf.int64)}
+
+	for feature_name in additional_features:
+		feature_map[feature_name] = tf.FixedLenFeature([feature_sizes[feature_name]], tf.float32)
+
+	features = tf.parse_single_example(serialized_example, feature_map)
+	concat_feature = tf.concat([features[name] for name in additional_features], axis = 0)
+	return features["labels"].values, concat_feature
+
+if __name__ == "__main__":
+
+	if len(sys.argv) > 3:
+		filepattern = sys.argv[1]
+		additional_features = [s.strip() for s in sys.argv[2].split(",")]
+		feature_sizes = [int(s) for s in sys.argv[3].split(",")]
+		feature_sizes = dict(zip(additional_features, feature_sizes))
+	else:
+		sys.exit("Usage: python %s filepattern additional_features feature_sizes")
+
+	with tf.Session() as sess:
+		label, feature = read_and_decode_single_example(filepattern, additional_features, feature_sizes)
+		global_init = tf.variables_initializer(tf.global_variables(), name = "global_init")
+		local_init = tf.variables_initializer(tf.local_variables(), name = "local_init")
+		sess.run(global_init)
+		sess.run(local_init)
+		tf.train.start_queue_runners(sess = sess)
+		i = 0
+		while True:
+			l_val, f_val = sess.run([label, feature])
+			l_list = l_val.flatten().tolist()
+			f_list = f_val.flatten().tolist()
+			l_len = len(l_list)
+			f_len = len(f_list)
+			string = "%d %s %d %s" % (
+					l_len, " ".join(map(str, l_list)),
+					f_len, " ".join(map(str, f_list)),
+					)
+			print string
---
+++ src/raw-knn/filetransformer.sh
@@ -0,0 +1,24 @@
+
+num=0
+for filename in $(ls /Youtube-8M/video/train); do
+	if [[ $filename =~ ^train ]]; then
+		num=$(( $num + 1 ))
+		echo "echo 'processing train file no.$num' && (CUDA_VISIBLE_DEVICES='' python filetransformer.py /Youtube-8M/video/train/$filename mean_rgb 1024 | gzip -c > ~/Youtube-8M/video/train/${filename}.vec.gz)"
+	fi
+done
+
+num=0
+for filename in $(ls /Youtube-8M/video/validate); do
+	if [[ $filename =~ ^validate ]]; then
+		num=$(( $num + 1 ))
+		echo "echo 'processing validate file no.$num' && (CUDA_VISIBLE_DEVICES='' python filetransformer.py /Youtube-8M/video/validate/$filename mean_rgb 1024 | gzip -c > ~/Youtube-8M/video/validate/${filename}.vec.gz)"
+	fi
+done
+
+num=0
+for filename in $(ls /Youtube-8M/video/test); do
+	if [[ $filename =~ ^test ]]; then
+		num=$(( $num + 1 ))
+		echo "echo 'processing test file no.$num' && (CUDA_VISIBLE_DEVICES='' python filetransformer.py /Youtube-8M/video/test/$filename mean_rgb 1024 | gzip -c > ~/Youtube-8M/video/test/${filename}.vec.gz)"
+	fi
+done
