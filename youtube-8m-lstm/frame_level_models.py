68342ef6d978fa12de84d723d59af6efe6da600d
Heda Wang <whd.thu@gmail.com>
2017-03-13 20:32:49 +0800

    add device description to LSTMMeanInput Model

--- youtube-8m-lstm/frame_level_models.py
+++ youtube-8m-lstm/frame_level_models.py
@@ -343,6 +343,56 @@ class LstmPoolingModel(models.BaseModel):
         vocab_size=vocab_size,
         **unused_params)
 
+class LstmWithMeanInputModel(models.BaseModel):
+
+  def create_model(self, model_input, vocab_size, num_frames, **unused_params):
+    """Creates a model which uses a stack of LSTMs to represent the video.
+
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+    lstm_size = int(FLAGS.lstm_cells)
+    number_of_layers = FLAGS.lstm_layers
+
+    with tf.device("/gpu:0"):
+      ## Batch normalize the input
+      stacked_lstm = tf.contrib.rnn.MultiRNNCell(
+              [
+                  tf.contrib.rnn.BasicLSTMCell(
+                      lstm_size, forget_bias=1.0, state_is_tuple=False)
+                  for _ in range(number_of_layers)
+                  ],
+              state_is_tuple=False)
+
+    loss = 0.0
+    with tf.variable_scope("RNN"):
+      outputs, state = tf.nn.dynamic_rnn(stacked_lstm, model_input,
+                                         sequence_length=num_frames,
+                                         time_major=False,
+                                         dtype=tf.float32)
+
+    with tf.device("/gpu:0"):
+      mean_input = tf.reduce_mean(model_input, axis = 1)
+      final_output = tf.concat([mean_input, state], axis = 1)
+
+    with tf.device("/cpu:0"):
+      aggregated_model = getattr(video_level_models,
+                                 FLAGS.video_level_classifier_model)
+      predictions = aggregated_model().create_model(
+          model_input=final_output,
+          vocab_size=vocab_size,
+          **unused_params)
+    return predictions
+
 class LstmWithPoolingModel(models.BaseModel):
 
   def create_model(self, model_input, vocab_size, num_frames, **unused_params):
