89d758df735c18997ad7f5ab02e49413312106eb
Heda Wang <whd.thu@gmail.com>
2017-03-13 16:26:27 +0800

    add bi-directional lstm

--- youtube-8m-lstm/frame_level_models.py
+++ youtube-8m-lstm/frame_level_models.py
@@ -239,6 +239,48 @@ class LstmModel(models.BaseModel):
         vocab_size=vocab_size,
         **unused_params)
 
+class BiLstmModel(models.BaseModel):
+
+  def create_model(self, model_input, vocab_size, num_frames, **unused_params):
+    """Creates a model which uses a stack of LSTMs to represent the video.
+
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+    lstm_size = int(FLAGS.lstm_cells)
+    number_of_layers = FLAGS.lstm_layers
+
+    ## Batch normalize the input
+    f_cell = tf.contrib.rnn.BasicLSTMCell(
+                lstm_size, forget_bias=1.0, state_is_tuple=False)
+    b_cell = tf.contrib.rnn.BasicLSTMCell(
+                lstm_size, forget_bias=1.0, state_is_tuple=False)
+
+    loss = 0.0
+    with tf.variable_scope("RNN"):
+      outputs, states = tf.nn.bidirectional_dynamic_rnn(cell_fw = f_cell, cell_bw = b_cell, 
+                                         inputs = model_input,
+                                         sequence_length=num_frames,
+                                         dtype=tf.float32)
+    state_fw, state_bw = states
+    state = tf.concat([state_fw, state_bw], axis = 1)
+
+    aggregated_model = getattr(video_level_models,
+                               FLAGS.video_level_classifier_model)
+    return aggregated_model().create_model(
+        model_input=state,
+        vocab_size=vocab_size,
+        **unused_params)
+
 class LstmParallelModel(models.BaseModel):
 
   def create_model(self, model_input, vocab_size, num_frames, **unused_params):
