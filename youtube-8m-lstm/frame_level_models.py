16c77d6aaa2a96907deb6cd1f2d74cf1228a3d8e
Heda Wang <whd.thu@gmail.com>
2017-03-24 10:53:01 +0800

    fixed a merge error

--- youtube-8m-lstm/frame_level_models.py
+++ youtube-8m-lstm/frame_level_models.py
@@ -496,7 +496,7 @@ class LstmWithPoolingModel(models.BaseModel):
                                          time_major=False,
                                          swap_memory=FLAGS.rnn_swap_memory,
                                          dtype=tf.float32)
-      num_frames_matrix = tf.maximum(tf.cast(tf.expand_dims(num_frames, axis=1), dtype=tf.float32), tf.ones([batch_size, 1]))
+      num_frames_matrix = tf.maximum(tf.cast(tf.expand_dims(num_frames, axis=1), dtype=tf.float32), 1.0)
       pooling_output = tf.reduce_sum(outputs, axis = 1) / num_frames_matrix
       final_output = tf.concat([pooling_output, state], axis = 1)
 
@@ -833,49 +833,6 @@ class LstmAttentionModel(models.BaseModel):
         vocab_size=vocab_size,
         **unused_params)
 
-class DeepLstmModel(models.BaseModel):
-
-  def create_model(self, model_input, vocab_size, num_frames, **unused_params):
-    """Creates a model which uses a stack of LSTMs to represent the video.
-
-    Args:
-      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
-                   input features.
-      vocab_size: The number of classes in the dataset.
-      num_frames: A vector of length 'batch' which indicates the number of
-           frames for each video (before padding).
-
-    Returns:
-      A dictionary with a tensor containing the probability predictions of the
-      model in the 'predictions' key. The dimensions of the tensor are
-      'batch_size' x 'num_classes'.
-    """
-    lstm_size = int(FLAGS.lstm_cells)
-    number_of_layers = FLAGS.lstm_layers
-
-    ## Batch normalize the input
-    stacked_lstm = tf.contrib.rnn.MultiRNNCell(
-            [
-                tf.contrib.rnn.BasicLSTMCell(
-                    lstm_size / (2 ** layer_ind), forget_bias=1.0, state_is_tuple=False)
-                for layer_ind in range(number_of_layers)
-                ],
-            state_is_tuple=True)
-
-    loss = 0.0
-    with tf.variable_scope("RNN"):
-      outputs, state = tf.nn.dynamic_rnn(stacked_lstm, model_input,
-                                         sequence_length=num_frames, 
-                                         swap_memory=FLAGS.rnn_swap_memory,
-                                         dtype=tf.float32)
-      final_state = tf.concat(map(lambda x: x.c, state), axis = 1)
-
-    aggregated_model = getattr(video_level_models,
-                               FLAGS.video_level_classifier_model)
-    return aggregated_model().create_model(
-        model_input=final_state,
-        vocab_size=vocab_size,
-        **unused_params)
 
 class LstmMemoryModel(models.BaseModel):
 
@@ -905,6 +862,50 @@ class LstmMemoryModel(models.BaseModel):
                 for _ in range(number_of_layers)
                 ],
             state_is_tuple=True)
+
+    loss = 0.0
+    with tf.variable_scope("RNN"):
+      outputs, state = tf.nn.dynamic_rnn(stacked_lstm, model_input,
+                                         sequence_length=num_frames, 
+                                         swap_memory=FLAGS.rnn_swap_memory,
+                                         dtype=tf.float32)
+      final_state = tf.concat(map(lambda x: x.c, state), axis = 1)
+
+    aggregated_model = getattr(video_level_models,
+                               FLAGS.video_level_classifier_model)
+    return aggregated_model().create_model(
+        model_input=final_state,
+        vocab_size=vocab_size,
+        **unused_params)
+
+class DeepLstmModel(models.BaseModel):
+
+  def create_model(self, model_input, vocab_size, num_frames, **unused_params):
+    """Creates a model which uses a stack of LSTMs to represent the video.
+
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+    lstm_size = int(FLAGS.lstm_cells)
+    number_of_layers = FLAGS.lstm_layers
+
+    ## Batch normalize the input
+    stacked_lstm = tf.contrib.rnn.MultiRNNCell(
+            [
+                tf.contrib.rnn.BasicLSTMCell(
+                    lstm_size / (2 ** layer_ind), forget_bias=1.0, state_is_tuple=False)
+                for layer_ind in range(number_of_layers)
+                ],
+            state_is_tuple=False)
 
     loss = 0.0
     with tf.variable_scope("RNN"):
@@ -951,7 +952,7 @@ class LstmAdvancedModel(models.BaseModel):
     stacked_lstm = tf.contrib.rnn.MultiRNNCell(
             [
                 tf.contrib.rnn.LSTMCell(
-                    lstm_size, forget_bias=1.0, state_is_tuple=False, use_peepholes=True)
+                    lstm_size, forget_bias=1.0, state_is_tuple=False, use_peepholes=False)
                 for _ in range(number_of_layers)
                 ],
             state_is_tuple=False)
