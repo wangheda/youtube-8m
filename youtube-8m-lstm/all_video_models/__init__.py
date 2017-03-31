ef5ec81483e9d5f1ac3b8711aeca17a8ce66305c
Heda Wang <whd.thu@gmail.com>
2017-03-31 16:55:30 +0800

    add video level chain model

--- youtube-8m-lstm/all_video_models/__init__.py
+++ youtube-8m-lstm/all_video_models/__init__.py
@@ -1,3 +1,5 @@
 from logistic_model import *
 from moe_model import *
+from chain_moe_model import *
+from multitask_moe_model import *
 from stage2_logistic_model import *
---
+++ youtube-8m-lstm/all_video_models/multitask_moe_model.py
@@ -0,0 +1,48 @@
+import math
+import models
+import tensorflow as tf
+import utils
+from tensorflow import flags
+import tensorflow.contrib.slim as slim
+FLAGS = flags.FLAGS
+
+class MultiTaskMoeModel(models.BaseModel):
+  """A softmax over a mixture of logistic models (with L2 regularization)."""
+
+  def create_model(self, model_input, vocab_size, num_mixtures=None,
+                   l2_penalty=1e-8, sub_scope="", **unused_params):
+    num_verticals = FLAGS.num_verticals
+    vertical_predictions = self.sub_model(model_input, num_verticals, sub_scope=sub_scope+"-support")
+    main_predictions = self.sub_model(model_input, vocab_size, sub_scope=sub_scope+"-main")
+    return {"predictions": main_predictions, "vertical_predictions": vertical_predictions}
+
+  def sub_model(self, model_input, vocab_size, num_mixtures=None, 
+                l2_penalty=1e-8, sub_scope="", **unused_params):
+    num_mixtures = num_mixtures or FLAGS.moe_num_mixtures
+
+    gate_activations = slim.fully_connected(
+        model_input,
+        vocab_size * (num_mixtures + 1),
+        activation_fn=None,
+        biases_initializer=None,
+        weights_regularizer=slim.l2_regularizer(l2_penalty),
+        scope="gates"+sub_scope)
+    expert_activations = slim.fully_connected(
+        model_input,
+        vocab_size * num_mixtures,
+        activation_fn=None,
+        weights_regularizer=slim.l2_regularizer(l2_penalty),
+        scope="experts"+sub_scope)
+
+    gating_distribution = tf.nn.softmax(tf.reshape(
+        gate_activations,
+        [-1, num_mixtures + 1]))  # (Batch * #Labels) x (num_mixtures + 1)
+    expert_distribution = tf.nn.sigmoid(tf.reshape(
+        expert_activations,
+        [-1, num_mixtures]))  # (Batch * #Labels) x num_mixtures
+
+    final_probabilities_by_class_and_batch = tf.reduce_sum(
+        gating_distribution[:, :num_mixtures] * expert_distribution, 1)
+    final_probabilities = tf.reshape(final_probabilities_by_class_and_batch,
+                                     [-1, vocab_size])
+    return final_probabilities
