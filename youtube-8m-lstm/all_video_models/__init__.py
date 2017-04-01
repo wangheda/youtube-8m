41f1dd03963af0c4cbe15f78943ba4064d5aac1b
Heda Wang <whd.thu@gmail.com>
2017-04-01 14:02:27 +0800

    add support relu model

--- youtube-8m-lstm/all_video_models/__init__.py
+++ youtube-8m-lstm/all_video_models/__init__.py
@@ -2,5 +2,6 @@ from logistic_model import *
 from moe_model import *
 from chain_moe_model import *
 from chain_main_relu_moe_model import *
+from chain_support_relu_moe_model import *
 from multitask_moe_model import *
 from stage2_logistic_model import *
