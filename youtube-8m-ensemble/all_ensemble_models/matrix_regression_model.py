346ed3a9587eb2d86beedd48297c034fcc29d979
Heda Wang <whd.thu@gmail.com>
2017-04-27 20:35:52 +0800

    add matrix_regression and moe_model

--- youtube-8m-ensemble/all_ensemble_models/matrix_regression_model.py
+++ youtube-8m-ensemble/all_ensemble_models/matrix_regression_model.py
@@ -6,11 +6,11 @@ from tensorflow import flags
 import tensorflow.contrib.slim as slim
 FLAGS = flags.FLAGS
 
-class LinearRegressionModel(models.BaseModel):
+class MatrixRegressionModel(models.BaseModel):
   """Logistic model with L2 regularization."""
 
   def create_model(self, model_input, vocab_size, l2_penalty=1e-8, original_input=None, **unused_params):
-    """Creates a linear regression model.
+    """Creates a matrix regression model.
 
     Args:
       model_input: 'batch' x 'num_features' x 'num_methods' matrix of input features.
@@ -20,11 +20,16 @@ class LinearRegressionModel(models.BaseModel):
       A dictionary with a tensor containing the probability predictions of the
       model in the 'predictions' key. The dimensions of the tensor are
       batch_size x num_classes."""
+    num_features = model_input.get_shape().as_list()[-2]
     num_methods = model_input.get_shape().as_list()[-1]
-    weight = tf.get_variable("ensemble_weight", 
+
+    weight1d = tf.get_variable("ensemble_weight1d", 
         shape=[num_methods],
         regularizer=slim.l2_regularizer(l2_penalty))
-    weight = tf.nn.softmax(weight)
-    output = tf.einsum("ijk,k->ij", model_input, weight)
+    weight2d = tf.get_variable("ensemble_weight2d", 
+        shape=[num_features, num_methods],
+        regularizer=slim.l2_regularizer(10 * l2_penalty))
+    weight = tf.nn.softmax(tf.einsum("ij,j->ij", weight2d, weight1d), dim=-1)
+    output = tf.einsum("ijk,jk->ij", model_input, weight)
     return {"predictions": output}
 
---
+++ youtube-8m-ensemble/ensemble_scripts/train-linear_model.sh
@@ -0,0 +1,17 @@
+model=$1
+conf=$2
+train_path=/Youtube-8M/model_predictions/ensemble_train
+
+train_data_patterns=""
+for d in $(cat $conf); do
+  train_data_patterns="${train_path}/${d}/*.tfrecord${train_data_patterns:+,$train_data_patterns}"
+done
+echo $train_data_patterns
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+      --train_dir="../model/${model}" \
+      --train_data_patterns="$train_data_patterns" \
+      --model=LinearRegressionModel \
+      --keep_checkpoint_every_n_hours=0.1 \
+      --batch_size=1024 \
+      --num_epochs=4
