f164b22c69787ace7940cc181a45cb2f5d0f3e02
Heda Wang <whd.thu@gmail.com>
2017-04-28 18:16:02 +0800

    add input moe

--- youtube-8m-ensemble/all_ensemble_models/__init__.py
+++ youtube-8m-ensemble/all_ensemble_models/__init__.py
@@ -1,5 +1,6 @@
 from logistic_model import *
 from moe_model import *
+from input_moe_model import *
 from linear_regression_model import *
 from matrix_regression_model import *
 from mean_model import *
---
+++ youtube-8m-ensemble/all_ensemble_models/input_moe_model.py
@@ -0,0 +1,32 @@
+import math
+import models
+import tensorflow as tf
+import utils
+from tensorflow import flags
+import tensorflow.contrib.slim as slim
+FLAGS = flags.FLAGS
+
+class InputMoeModel(models.BaseModel):
+  """A softmax over a mixture of logistic models (with L2 regularization)."""
+
+  def create_model(self,
+                   model_input,
+                   vocab_size,
+                   num_mixtures=None,
+                   l2_penalty=1e-8,
+                   sub_scope="",
+                   original_input=None, 
+                   **unused_params):
+
+    num_methods = model_input.get_shape().as_list()[-1]
+    num_features = model_input.get_shape().as_list()[-2]
+
+    gate_activations = slim.fully_connected(
+        original_input,
+        num_methods,
+        activation_fn=tf.nn.softmax,
+        weights_regularizer=slim.l2_regularizer(l2_penalty),
+        scope="gates"+sub_scope)
+
+    output = tf.einsum("ijk,ik->ij", model_input, gate_activations)
+    return {"predictions": output}
