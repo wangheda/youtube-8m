c0e7b48ece54406c160bf5ee051f97e172330e39
Heda Wang <whd.thu@gmail.com>
2017-05-04 23:38:02 +0800

    add infer biuni scripts

--- youtube-8m-lstm/infer_scripts/infer-biunilstm1024_moe8.sh
+++ youtube-8m-lstm/infer_scripts/infer-biunilstm1024_moe8.sh
@@ -1,6 +1,6 @@
 
 for part in test ensemble_train ensemble_validate train_samples; do 
-    CUDA_VISIBLE_DEVICES=1 python inference-pre-ensemble.py \
+    CUDA_VISIBLE_DEVICES=0 python inference-pre-ensemble.py \
 	      --output_dir="/Youtube-8M/model_predictions/${part}/biunilstm1024_moe4" \
         --model_checkpoint_path="../model/biunilstm1024_moe4/model.ckpt-94882" \
 	      --input_data_pattern="/Youtube-8M/data/frame/${part}/*.tfrecord" \
