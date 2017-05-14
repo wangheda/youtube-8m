e53c486dd59bac6e9aa13768183905c0ba71ff45
Heda Wang <whd.thu@gmail.com>
2017-05-14 10:37:57 +0800

    add positional cnn dcc

---
+++ youtube-8m-lstm/training_scripts/run-positional-cnn-deep-combine-chain.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES=0 python train.py \
+        --train_dir="../model/positional_cnn_deep_combine_chain/" \
+        --train_data_pattern="/Youtube-8M/data/frame/train/train*" \
+        --frame_features=True \
+        --feature_names="rgb,audio" \
+        --feature_sizes="1024,128" \
+        --model=PositionalCnnDeepCombineChainModel \
+        --label_loss=MultiTaskCrossEntropyLoss \
+        --batch_size=96 \
+        --multitask=True \
+        --support_type="label,label" \
+        --support_loss_percent=0.1 \
+        --moe_num_mixtures=4 \
+        --reweight=True \
+        --sample_vocab_file="resources/train.video_id.vocab" \
+        --sample_freq_file="resources/train.video_id.freq" \
+        --keep_checkpoint_every_n_hours=0.5 \
+        --num_readers=8 \
+        --deep_chain_layers=2 \
+        --deep_chain_relu_cells=128 \
+        --base_learning_rate=0.001
+
