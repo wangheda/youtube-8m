b5bba8f44d254f5d4bd10fb36e5f33b3ed8289c8
Heda Wang <whd.thu@gmail.com>
2017-05-28 20:20:42 +0800

    add infer distillation scripts

--- youtube-8m-ensemble/model_selection_scripts/greedy-selection-mean_model.sh
+++ youtube-8m-ensemble/model_selection_scripts/greedy-selection-mean_model.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 model_name=$1
 
 extend_step_script="${DIR}/extend-step-mean_model.sh"
-train_path=/Youtube-8M/model_predictions/ensemble_train
+train_path=/Youtube-8M/model_predictions_for_selection/ensemble_train
 model_path="${DIR}/../../model/${model_name}"
 all_models_conf="${model_path}/all_models.conf"
 
@@ -13,7 +13,7 @@ if [ -f $all_models_conf ]; then
   top_models_conf="${all_models_conf}"
   len_k_models_conf="${all_models_conf}"
 
-  for step in {1..19}; do 
+  for step in {1..30}; do 
 
       if [ $step -gt 1 ]; then
           len_k_models_conf="${model_path}/len_${step}_models.conf"
@@ -21,7 +21,7 @@ if [ -f $all_models_conf ]; then
       fi
 
       bash $extend_step_script $model_name ${len_k_models_conf} > ${model_path}/len_${step}_models.log
-      python ${DIR}/get_top_k.py --top_k=3 --log_file="${model_path}/len_${step}_models.log" --sorted_log_file="${model_path}/len_${step}_models.sorted.log" > ${model_path}/top_${step}_models.conf
+      python ${DIR}/get_top_k.py --top_k=2 --log_file="${model_path}/len_${step}_models.log" --sorted_log_file="${model_path}/len_${step}_models.sorted.log" > ${model_path}/top_${step}_models.conf
 
       top_models_conf="${model_path}/top_${step}_models.conf"
   done
