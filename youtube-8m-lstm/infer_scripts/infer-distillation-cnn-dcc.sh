c9b28e49dde099fb6ffe4d3bd5354dee5deea390
Heda Wang <whd.thu@gmail.com>
2017-05-29 09:50:40 +0800

    change batch size for distillation cnn dcc

--- youtube-8m-lstm/infer_scripts/infer-distillation-cnn-dcc.sh
+++ youtube-8m-lstm/infer_scripts/infer-distillation-cnn-dcc.sh
@@ -1,9 +1,8 @@
 
-for part in ensemble_train ensemble_validate test; do 
-#for part in train; do 
-  CUDA_VISIBLE_DEVICES=1 python inference-pre-ensemble.py \
-    --output_dir="/Youtube-8M/model_predictions/${part}/distillchain_cnn_dcc" \
-    --model_checkpoint_path="../model/distillchain_cnn_dcc/model.ckpt-74488" \
+for part in train; do 
+  CUDA_VISIBLE_DEVICES=0 python inference-pre-ensemble.py \
+    --output_dir="/Youtube-8M/model_predictions_local/${part}/distillation/distillchain_cnn_dcc" \
+    --model_checkpoint_path="../model/distillation/distillchain_cnn_dcc/model.ckpt-74488" \
     --input_data_pattern="/Youtube-8M/data/frame/${part}/*.tfrecord" \
     --frame_features=True \
     --feature_names="rgb,audio" \
@@ -15,6 +14,6 @@ for part in ensemble_train ensemble_validate test; do
     --deep_chain_layers=3 \
     --deep_chain_relu_cells=256 \
     --moe_num_mixtures=4 \
-    --batch_size=32 \
+    --batch_size=128 \
     --file_size=4096
 done
